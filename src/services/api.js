3f86719e867acdf90bac7bad7e9b77c9ed67495a
Breno Santos <brenosantos145@gmail.com>
2022-01-19 16:13:39 -0300

    Requisito 01 - completo - Necessita code review

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
   // Implemente aqui
+  const apiUrl = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(apiUrl).then((res) => res.json());
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId = '', query = '') {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const apiUrl = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const teste = await fetch(apiUrl).then((res) => res.json());
+  return teste;
 }
